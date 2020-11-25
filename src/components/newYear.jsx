37beaf83812f538c6ea3b6f479723d2ff23d34ce
mutua-mutinda <mutuamutinda@outlook.com>
2020-11-25 13:30:18 +0300

    changed some imports

--- src/components/newYear.jsx
+++ src/components/newYear.jsx
@@ -1,24 +1,27 @@
+
 const countDate = new Date('jan 1, 2021 00:00:00').getTime();
 
-export default function newYear() {
-            const now = new Date().getTime();
-            const gap = countDate - now;
+function newYear() {
+    const now = new Date().getTime();
+    const gap = countDate - now;
 
-            const second = 1000;
-            const minute = second * 60;
-            const hour = minute * 60;
-            const day = hour * 24;
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
 
-            const d = Math.floor(gap / (day));
-            const h = Math.floor((gap % (day)) / (hour));
-            const m = Math.floor((gap % (hour)) / (minute));
-            const s = Math.floor((gap % (minute)) / (second));
+    const d = Math.floor(gap / (day));
+    const h = Math.floor((gap % (day)) / (hour));
+    const m = Math.floor((gap % (hour)) / (minute));
+    const s = Math.floor((gap % (minute)) / (second));
 
-            document.querySelector('#day').innerText = d;
-            document.querySelector('#hour').innerText = h;
-            document.querySelector('#minute').innerText = m;
-            document.querySelector('#second').innerText = s;
-        }
+    document.querySelector('#day').innerText = d;
+    document.querySelector('#hour').innerText = h;
+    document.querySelector('#minute').innerText = m;
+    document.querySelector('#second').innerText = s;
+}
 setInterval(function(){
     newYear()
 },1000);
+
+ export default  newYear;
