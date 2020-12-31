38734702151ff0501182910a313bab1102d2564d
mutua-mutinda <mutuamutinda@outlook.com>
2021-01-01 00:36:24 +0300

    changed the countdown to 2021

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,27 @@
 import React from "react";
-import './newYear';
+import "./newYear";
 import "./css/App.css";
 
-   
 export default function App() {
   return (
     <>
-    <div className="container">
+      <div className="container">
         <div className="imgBx"></div>
         <div className="countdownBx">
-            <h2>Count Down To New Year <br/> <span>20<i>20</i></span></h2>
-            <div className="countdown">
-                <div id="day">D</div>
-                <div id="hour">H</div>
-                <div id="minute">M</div>
-                <div id="second">S</div>
-            </div>
+          <h2>
+            Count Down To New Year <br />{" "}
+            <span>
+              20<i>21</i>
+            </span>
+          </h2>
+          <div className="countdown">
+            <div id="day">D</div>
+            <div id="hour">H</div>
+            <div id="minute">M</div>
+            <div id="second">S</div>
+          </div>
+        </div>
       </div>
-    </div>
     </>
-    );
+  );
 }
--- src/components/newYear.jsx
+++ src/components/newYear.jsx
@@ -1,27 +1,26 @@
-
-const countDate = new Date('jan 1, 2021 00:00:00').getTime();
+const countDate = new Date("jan 1, 2022 00:00:00").getTime();
 
 function newYear() {
-    const now = new Date().getTime();
-    const gap = countDate - now;
+  const now = new Date().getTime();
+  const gap = countDate - now;
 
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    const d = Math.floor(gap / (day));
-    const h = Math.floor((gap % (day)) / (hour));
-    const m = Math.floor((gap % (hour)) / (minute));
-    const s = Math.floor((gap % (minute)) / (second));
+  const d = Math.floor(gap / day);
+  const h = Math.floor((gap % day) / hour);
+  const m = Math.floor((gap % hour) / minute);
+  const s = Math.floor((gap % minute) / second);
 
-    document.querySelector('#day').innerText = d;
-    document.querySelector('#hour').innerText = h;
-    document.querySelector('#minute').innerText = m;
-    document.querySelector('#second').innerText = s;
+  document.querySelector("#day").innerText = d;
+  document.querySelector("#hour").innerText = h;
+  document.querySelector("#minute").innerText = m;
+  document.querySelector("#second").innerText = s;
 }
-setInterval(function(){
-    newYear()
-},1000);
+setInterval(function () {
+  newYear();
+}, 1000);
 
-export default  newYear;
+export default newYear;
